5031c008ac269406e5d60c88c98f009ee5b4f97c
phil jones <interstar@gmail.com>
2012-01-04 11:15:10 +0000

    comments

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
 
 bool flag = true;
 
-// Sublime Loop (See http://www.sublimeloop.com/ for details )
+
 /*
+// Sublime Loop (See http://www.sublimeloop.com/ for details )
 int notes[64] = {50,53,50,53, 52,55,52,55, 48,52,48,52, 50,54,50,54, 
                  50,53,50,53, 52,55,52,55, 48,52,48,52, 50,54,50,54,
                  50,53,50,53, 52,55,52,55, 48,52,48,52, 50,54,50,54,                 
@@ -22,7 +23,9 @@ int bassNotes[16] = {46, 48, 45, 42,
 };
 */
 
-// new test chords / seq
+// new test sequence
+
+// notes in {pitch,duration} format
 int notes[11][2] = { {50,96},
                     {53,48},
                     {55,96},
@@ -36,12 +39,16 @@ int notes[11][2] = { {50,96},
                     {65,96}
                     };
 
+// these are chord offsets, they're used for the Voice which is playing a base-note plus these offsets
 int chordOffs[4]={0,3,7,10};                    
-int bassNotes[4][2] = { {50,192},
+
+// the actual sequence of base-notes for the chord voice
+int chordNotes[4][2] = { {50,192},
                    {52,192},
                    {57,192},
                    {52,192} };
 
+// a "high-hat" pattern
 int highNotes[16][2] = { {100,24},
                        {100,24},
                        {100,24},
@@ -60,7 +67,7 @@ int highNotes[16][2] = { {100,24},
                        {100,24} };
 
 
-Sequencer seq, bass, hat;
+Sequencer seq, chords, hat;
 
 Instrument inChords, inSolo, inHat;
 
@@ -70,26 +77,28 @@ void setup() {//some inits
 
     printf("start\n");
     
-    inChords.start(SIN,RAMP,4,chordOffs);
-    inChords.setParams(1,9987,1,7001,1,90001,0.02,0);
-    inChords.setEnv(0.5, 0.999975, 5000);
-    inChords.voice.volume = 1.1;
+    inChords.start(SIN,RAMP,4,chordOffs); // creates an Instrument with Sin and Ramp waves, 4 actual voices, spaced with chordOffs
+    inChords.setParams(1,9987,1,7001,1,90001,0.02,0); // sets up the sound parameters for this Instrument 
+    inChords.setEnv(0.5, 0.999975, 5000); // sets envelope for the Instrument
+    inChords.voice.volume = 1.1; // sets volume
         
-    inSolo.start(SIN,SIN,1,norm);
-    inSolo.setParams(0,9987,1,7001,0,1001,0,-12);
-    inSolo.setEnv(0.05, 0.909975, 5000);
+    inSolo.start(SIN,SIN,1,norm); // single voiced Instrument, made with 2 Sin waves
+    inSolo.setParams(0,9987,1,7001,0,1001,0,-12); // sets up the sound parameters for this Instrument
+    inSolo.setEnv(0.05, 0.909975, 5000); // sets envelope for the Instrument ... etc.
     inSolo.voice.volume = 0.5;
     
     inHat.start(SIN,RAMP,1,norm);
     inHat.setParams(1,48,1,19999,1,1000,0.1,0);
     inHat.setEnv(0.01, 0.99, 1000);
-         
+        
+    // sets up the three sequences 
     seq.start(11,notes,250);
-    bass.start(4,bassNotes,250);
+    chords.start(4,chordNotes,250);
     hat.start(16,highNotes,250);
     
+    // push the sequences to the beginning.
     seq.advanceNote();
-    bass.advanceNote();
+    chords.advanceNote();
     hat.advanceNote();
  
     flag = false;
@@ -99,7 +108,7 @@ void play(double *output) {
     if (flag) { setup(); }
 
     double o,o2,o3;
-    o = inChords.next(bass.currentNote,bass.trigger);
+    o = inChords.next(chords.currentNote,chords.trigger);
     o2 = inSolo.next(seq.currentNote,seq.trigger);
     o3 = inHat.next(hat.currentNote,hat.trigger);
     
@@ -107,6 +116,6 @@ void play(double *output) {
 	output[1]=(o+o2+o3)/3;	
 	
     seq.step();
-    bass.step();
+    chords.step();
     hat.step();
 }
